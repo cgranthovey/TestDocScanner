e9f55f35cee88476f6f91c0a5b91691de18b5f37
Chris Hovey <cgranthovey@gmail.com>
2020-08-17 13:31:08 -0600

    Initial Commit

---
+++ TestDocScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestDocScanner
+//
+//  Created by Christopher Hovey on 8/17/20.
+//  Copyright © 2020 Chris Hovey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
